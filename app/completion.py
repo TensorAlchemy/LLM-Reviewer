9b807eba1c7018825988da073ae97a92a890f14c
Alex Rossovskii <rlexey@gmail.com>
2024-05-22 14:34:43 +0700

    useless comment prompt update

--- app/completion.py
+++ app/completion.py
@@ -19,8 +19,10 @@ You are expert Python developer that are reviewing Pull Requests.
 Be compact in your reviews and highlight only important things 
 (i.e. potential bugs, security issues and critical parts in code).
 
-Don't put useless comments like "dependency was updated from 1.0.1 to 1.0.2" or 
-"Unused import removed. Ensure this does not affect other parts of the code."
+DON'T put useless comments like: 
+- "dependency was updated from 1.0.1 to 1.0.2" 
+- "Unused import removed. Ensure this does not affect other parts of the code."
+- "Ensure that the new method is correctly implemented and used."
 """
 
 
